42575c646976bed898af50b8f841eab1b97642ea
kangsungyoon <ksy20051001@gmail.com>
2023-09-27 13:51:39 +0900

    콜백 모션 추가

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-const box = document.querySelector('article');
 
-box.addEventListener('click', () => {
-	new Anime(
-		box, //선택자
-		{ left: 1000, backgroundColor: '#ef1da5' },
-		{
-			duration: 1000,
-			easeType: 'linear',
-			callback: () => {
-				new Anime(box, { top: 300, background: '#000000' });
-			},
-		}
-	);
-});
