5bc58fd99f2af677e8b448336b6fe6bb3e27f7b7
kangsungyoon <ksy20051001@gmail.com>
2023-09-27 10:13:24 +0900

    두번째 섹션에 도달했을때 모션 시작

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 /*
-  window.scrolly : 브라우저 스크롤시 스크롤 되는 거리값 (동적)
-  DOM.offset().top : 전체페이지에 특정 DOM의 세로 이치값 (정적)
-  */
-
+  window.scrollY : 브라우저 스크롤시 스크롤되는 거리값 (동적)
+  DOM.offsetTop : 전체 페이지에 특정 DOM의 세로 위치값 (정적)
+*/
 const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
+const h1_2 = secs[1].querySelector('h1');
 
 window.addEventListener('scroll', () => {
 	const scroll = window.scrollY;
+	const scroll_2 = scroll - secs[1].offsetTop;
+
 	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1 + scroll / 200})`;
 	h1.style.opacity = 1 - scroll / 400;
+
+	if (scroll >= secs[1].offsetTop) {
+		h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
+			1 + scroll_2 / 200
+		})`;
+		h1_2.style.opacity = 1 - scroll_2 / 400;
+	}
 });
