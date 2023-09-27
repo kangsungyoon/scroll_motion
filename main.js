9f2f293f5f0fd624a95e398cca508648c4760b91
kangsungyoon <ksy20051001@gmail.com>
2023-09-27 10:27:46 +0900

    스크롤값 초기화시 위치값 강제 보정

--- main.js
+++ main.js
@@ -5,18 +5,27 @@
 const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
 const h1_2 = secs[1].querySelector('h1');
+//baseLine은 박스의 모션 시작시점을 기존 박스 영역에서 300만큼 위로 올림
+const baseLine = -300;
 
 window.addEventListener('scroll', () => {
 	const scroll = window.scrollY;
-	const scroll_2 = scroll - secs[1].offsetTop;
+	// 박스에 실 적용되는 스크롤 수치값을 반대로 300만큼 더해줘야됨
+	const scroll_2 = scroll - secs[1].offsetTop - baseLine;
+	const initScroll = 0;
 
 	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1 + scroll / 200})`;
 	h1.style.opacity = 1 - scroll / 400;
 
-	if (scroll >= secs[1].offsetTop) {
+	if (scroll >= secs[1].offsetTop + baseLine) {
 		h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
 			1 + scroll_2 / 200
 		})`;
 		h1_2.style.opacity = 1 - scroll_2 / 400;
+	} else {
+		h1_2.style.transform = `translateX(${initScroll}px) rotate(${initScroll}deg) scale(${
+			1 + initScroll / 200
+		})`;
+		h1_2.style.opacity = 1 - initScroll / 400;
 	}
 });
